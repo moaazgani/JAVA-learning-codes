0c9859e2db1dc61a977e1fc07dd207b3c305eb24
Moaaz Gani <moaazgani@gmail.com>
2026-05-31 16:59:08 +0530

    Refactor classes.java to remove commented code

--- OOPs/classes.java
+++ OOPs/classes.java
@@ -3,7 +3,7 @@ class pen{
     String type;
 
     public void write(){
-        System.out.println("Pen is ready");
+        System.out.println("Pen is ready ");
     }
 
     public void printcolor(){
